57f719fc657418e79191845be0534f33d740c39f
Cyprien AVICO <95435335+Akmot9@users.noreply.github.com>
2023-03-26 15:25:20 +0200

    Update install_jenkins.sh
    error handeling

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,20 +1,41 @@
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/jenkins.gpg
-# This command downloads the Jenkins repository key using the curl command and pipes the output to the gpg command 
-# to convert it to a format that can be used by the apt-key command. The converted key is then saved to the /etc/apt/trusted.gpg.d/jenkins.gpg file,
-# which is used by apt to verify the authenticity of packages downloaded from the Jenkins repository.
+#!/bin/bash
 
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-# This command adds the Jenkins repository to your system's apt sources list by creating a new file called /etc/apt/sources.list.d/jenkins.list and 
-# writing the repository URL (https://pkg.jenkins.io/debian-stable binary/) to it.
+# This script installs Jenkins on an Ubuntu system
 
-sudo apt update
-# This command updates the package index on your system to include the packages available from the Jenkins repository that you just added.
+# Download and install the Jenkins repository key
+if ! curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/jenkins.gpg; then
+    echo "Error: Unable to download and install the Jenkins repository key"
+    exit 1
+fi
 
-sudo apt install jenkins
-# This command installs the Jenkins package on your system using apt.
+# Add the Jenkins repository to the apt sources list
+if ! sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'; then
+    echo "Error: Unable to add the Jenkins repository to the apt sources list"
+    exit 1
+fi
 
-sudo systemctl start jenkins
-# This command starts the Jenkins service on your system.
+# Update the package index
+if ! sudo apt update; then
+    echo "Error: Unable to update the package index"
+    exit 1
+fi
 
-sudo systemctl enable jenkins
-# This command configures the Jenkins service to start automatically at system boot.
+# Install Jenkins
+if ! sudo apt install -y jenkins; then
+    echo "Error: Unable to install Jenkins"
+    exit 1
+fi
+
+# Start the Jenkins service
+if ! sudo systemctl start jenkins; then
+    echo "Error: Unable to start the Jenkins service"
+    exit 1
+fi
+
+# Enable the Jenkins service to start automatically at system boot
+if ! sudo systemctl enable jenkins; then
+    echo "Error: Unable to enable the Jenkins service to start automatically at system boot"
+    exit 1
+fi
+
+echo "Jenkins installation completed successfully!"
